5978cc25e49d9af45ad62c1d71a11bbde8820d70
bconti123 <bconti00@gmail.com>
2022-06-13 13:38:14 -0700

    Working on the online shop

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # shopify_project test
+# I just started to work on online shop in Shopify. I am excited to create my own online shop!
---
+++ hydrogen-app/hydrogen.config.js
@@ -0,0 +1,9 @@
+import {defineConfig} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    storeDomain: '',
+    storefrontToken: '',
+    storefrontApiVersion: '2022-07',
+  },
+});
