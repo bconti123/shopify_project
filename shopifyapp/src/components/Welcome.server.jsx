0688b284c233bd98394bae4abd3e28368cc8b4de
bconti123 <bconti00@gmail.com>
2022-06-16 14:15:22 -0700

    Still learning

--- shopifyapp/src/components/Welcome.server.jsx
+++ shopifyapp/src/components/Welcome.server.jsx
@@ -33,6 +33,7 @@ function DocsButton({url, label}) {
     >
       {label}
       <ExternalIcon />
+     
     </a>
   );
 }
@@ -61,37 +62,10 @@ function StorefrontInfo() {
     `${count} ${noun}${count === 1 ? '' : suffix}`;
   return (
     <div className="bg-white p-12 shadow-xl rounded-xl text-gray-900">
-      <p className="text-md font-medium uppercase mb-4">Connected Storefront</p>
-      <h2 className="text-2xl font-bold mb-4">{shopName}</h2>
-      <p className="text-md">
-        {pluralize(totalProducts, 'Product')}
-        {', '}
-        {pluralize(totalCollections, 'Collection')}
+      
+      <p className="text-md font-medium uppercase mb-4">
+        Left Card
       </p>
-      {totalProducts === 0 && totalCollections === 0 && (
-        <div className="py-2 px-3 bg-red-100 text-md">
-          Use the{' '}
-          <a
-            href="https://shopify.dev/apps/tools/cli/getting-started"
-            className="text-primary font-mono font-bold underline"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Shopify CLI
-          </a>{' '}
-          to populate sample products and collections.
-        </div>
-      )}
-      <hr className="my-4" />
-      <a
-        href="https://shopify.dev/custom-storefronts/hydrogen/getting-started/create#step-2-update-information-about-your-shopify-storefront"
-        className="text-md inline-flex items-center text-blue-700 font-medium hover:underline"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Change your storefront access token
-        <ExternalIcon />
-      </a>
     </div>
   );
 }
@@ -113,32 +87,11 @@ function TemplateLinks() {
   return (
     <div className="bg-white p-12 md:p-12 shadow-xl rounded-xl text-gray-900">
       <p className="text-md font-medium uppercase mb-4">
-        Explore the templates
+        Right Card
       </p>
       <ul>
-        <li className="mb-4">
-          <Link
-            to={`/collections/${firstCollection}`}
-            className="text-md font-medium text-blue-700 hover:underline"
-          >
-            Collection template
-          </Link>
-        </li>
-        <li className="mb-4">
-          <Link
-            to={`/products/${firstProduct}`}
-            className="text-md font-medium text-blue-700 hover:underline"
-          >
-            Product template
-          </Link>
-        </li>
         <li>
-          <Link
-            to="/error-page"
-            className="text-md font-medium text-blue-700 hover:underline"
-          >
-            404 template
-          </Link>
+
         </li>
       </ul>
     </div>
